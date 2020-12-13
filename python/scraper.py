0fc939346b67f5ca4b31ab9fdd291066eb414cad
Bartek_makowski <mako19895@gmail.com>
2020-12-13 14:24:32 +0000

    python files moved to python folder and first methods written in scraper.py

---
+++ python/scraper.py
@@ -0,0 +1,15 @@
+class JobScrape():
+    def __init__(self,):
+        pass
+
+    def get_jobs():
+        pass
+
+    def _scrape_site():
+        pass
+
+    def _get_description():
+        pass
+
+    def _format_monster():
+        pass

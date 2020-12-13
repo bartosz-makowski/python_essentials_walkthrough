089a8a3ca3a66c8fc3462a3d00d4b40be923c3f7
Bartek_makowski <mako19895@gmail.com>
2020-12-13 22:19:48 +0000

    second tutorial finished

--- python/scraper.py
+++ python/scraper.py
@@ -1,23 +1,25 @@
 class JobScrape():
     def __init__(self, site_name,):
-        """Having the job sites in a list means 
-        we can check on initialisation and 
-        throw an error if the site is not available """"
+        """Having the job sites in a list means we can check on initialisation and throw an error if the site is not available"""
         
         sites =[{"monster": {
                     "url":"https://www.monster.co.uk/jobs/search/",
                     "query_format" : "?q={keywords}}&where={city}}&cy={country}",
                     'results' : '#ResultsContainer'
         }}]
-
-    def get_jobs():
+        try:
+            self.site_name = site_name
+            self.site_data = [site[site_name] for site in sites if site_name in site][0]
+        except IndexError:
+            raise ValueError(f'{site_name} is not found or not supported yet!')
+    #def get_jobs():
         pass
 
-    def _scrape_site():
+    #def _scrape_site():
         pass
 
-    def _get_description():
+    #def _get_description():
         pass
 
-    def _format_monster():
+    #def _format_monster():
         pass
